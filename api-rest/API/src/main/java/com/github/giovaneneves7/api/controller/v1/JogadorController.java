5b2d6072f3d79b4ab9d3baf2c6620791be876239
NekoYasha <autornekoyasha@gmail.com>
2023-12-26 09:57:56 -0300

    refactor: Limpando controller

--- api-rest/API/src/main/java/com/github/giovaneneves7/api/controller/v1/JogadorController.java
+++ api-rest/API/src/main/java/com/github/giovaneneves7/api/controller/v1/JogadorController.java
@@ -1,15 +1,13 @@
 package com.github.giovaneneves7.api.controller.v1;
 
+import com.github.giovaneneves7.api.controller.v1.util.ResultError;
 import com.github.giovaneneves7.api.entity.jogador.dto.JogadorPostRequestDto;
 import com.github.giovaneneves7.api.entity.jogador.service.IJogadorService;
 import com.github.giovaneneves7.api.entity.jogador.model.Jogador;
 
 import com.github.giovaneneves7.api.infrastructure.util.ObjectMapperUtil;
-import org.modelmapper.ModelMapper;
-import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -41,31 +39,21 @@ public class JogadorController {
     public ResponseEntity<?> salvarJogador(@RequestBody JogadorPostRequestDto jogadorDto,
                                            BindingResult result){
 
-        return ResponseEntity.status(HttpStatus.OK)
-                .body(this._jogadorService.salvarJogador(this._objectMapperUtil.map(jogadorDto, new Jogador())));
+        return (result.hasErrors())
+                ? ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body(ResultError.getResultErrors(result))
+                : ResponseEntity.status(HttpStatus.OK)
+                    .body(this._jogadorService.saveJogador(this._objectMapperUtil.map(jogadorDto, new Jogador())));
     }
 
     @GetMapping(path = "/jogadores", produces = "application/json")
     public ResponseEntity<?> listarJogadores(){
 
         return ResponseEntity.status(HttpStatus.OK)
-                .body(this._jogadorService.listarJogadores());
+                .body(this._jogadorService.getAllJogadores());
 
     }
 
-    @GetMapping(path = "/jogadores/jogador/{id}", produces = "application/json")
-    public ResponseEntity<?> pegarJogadorPeloId(@PathVariable("id") Long id){
-
-        // TODO: Adicionar lógica.
-        return null;
-    }
-
-    @PutMapping(path = "/jogadores/jogador", consumes = "application/json", produces = "application/json")
-    public ResponseEntity<?> atualizarJogador(/*TODO: Criar DTO para requesição PUT*/){
-
-        // TODO: Adicionar lógica.
-        return null;
-    }
     @DeleteMapping(path = "/jogadores/jogador/{id}", produces = "application/json")
     public ResponseEntity<?> deletarJogadorPorId(@PathVariable("id") Long id){
 

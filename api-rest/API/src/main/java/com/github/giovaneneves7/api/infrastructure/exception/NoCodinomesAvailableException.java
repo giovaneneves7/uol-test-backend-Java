ec081b6970ef25d3d80845fd4a90c666ed03e6f7
NekoYasha <autornekoyasha@gmail.com>
2023-12-26 09:18:25 -0300

    feat: Criando exceção que será disparada quando não houver codinomes válidos

---
+++ api-rest/API/src/main/java/com/github/giovaneneves7/api/infrastructure/exception/NoCodinomesAvailableException.java
@@ -0,0 +1,9 @@
+package com.github.giovaneneves7.api.infrastructure.exception;
+
+public class NoCodinomesAvailableException extends Exception{
+
+    public NoCodinomesAvailableException(String message) {
+        super(message);
+    }
+
+}
